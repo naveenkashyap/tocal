ecef2c7c2f4b0a86ca7d8e41d4b04debd8e34b8c
Naveen Kashyap <naveen@instabase.com>
2021-02-10 18:21:26 -0800

    Use abstractions in main

--- tocal/__main__.py
+++ tocal/__main__.py
@@ -1,42 +1,28 @@
 import datetime
-import pickle
 import os
-import logging
 
 from typing import List
 
-from googleapiclient.discovery import build  # type: ignore
-from google_auth_oauthlib.flow import InstalledAppFlow # type: ignore
-from google.auth.transport.requests import Request # type: ignore
-from google.oauth2.credentials import Credentials # type: ignore 
-
+from .service import Service
+from .calendar import Calendar, Event
 
 PACKAGE_ROOT: str = os.path.dirname(os.path.abspath(__file__))
-SECRETS_PATH: str = os.path.join(PACKAGE_ROOT, 'secrets', 'google', 'client_secret.json')
-CREDS_PATH: str = os.path.join(PACKAGE_ROOT, 'creds', 'google', 'creds.pickle')
+SECRETS_PATH: str = os.path.join(
+    PACKAGE_ROOT, "secrets", "google", "client_secret.json"
+)
+CREDS_PATH: str = os.path.join(PACKAGE_ROOT, "creds", "google", "creds.pickle")
 
-SCOPES: List[str] = ['https://www.googleapis.com/auth/calendar.events']
+SCOPES: List[str] = ["https://www.googleapis.com/auth/calendar.events"]
 
-def get_creds() -> Credentials:
-    creds = None
-    if os.path.exists(CREDS_PATH):
-        with open(CREDS_PATH, 'rb') as f:
-            creds =  pickle.load(f)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(SECRETS_PATH, scopes=SCOPES)
-            creds = flow.run_local_server()
-        with open(CREDS_PATH, 'wb') as f:
-            pickle.dump(creds, f)
-    
-    return creds
 
 def main():
-    creds = get_creds()
-    service = build('calendar', 'v3', credentials=creds)
+    service = Service(
+        creds_path=CREDS_PATH,
+        secrets_path=SECRETS_PATH,
+        scopes=SCOPES,
+        service_name="calendar",
+        service_version="v3",
+    ).service
 
     # Define the start and end times of the current day
     #   start = now
@@ -48,21 +34,36 @@ def main():
 
     now = datetime.datetime.utcnow()
     tomorrow = now + day_length
-    now = now.isoformat() + 'Z'
-    tomorrow = tomorrow.isoformat() + 'Z'
+    now = now.isoformat() + "Z"
+    tomorrow = tomorrow.isoformat() + "Z"
 
     # Call the Calendar API
-    print('Getting your events for the rest of the day')
+    print("Getting your events for the rest of the day")
 
-    events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=tomorrow,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            timeMax=tomorrow,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    owner = events_result.get("summary", "unknown")
+    events = events_result.get("items", [])
 
+    cal = Calendar(owner)
     if not events:
-        print('No upcoming events found.')
+        print("No upcoming events found.")
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        end = event["end"].get("dateTime", event["end"].get("date"))
+        start_datetime = datetime.datetime.fromisoformat(start)
+        end_datetime = datetime.datetime.fromisoformat(end)
+        duration = end_datetime - start_datetime
+        event = Event(duration.total_seconds(), event)
+        cal.add_event(event)
 
